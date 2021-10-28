dcaf6c6bb4b3a2741bc033f9088bcb7eacecf4c0
PALLAVI RANI <56119758+Pallavi168@users.noreply.github.com>
2021-10-28 16:59:45 +0530

    Update index.js

--- Index.js
+++ Index.js
@@ -1,10 +1,10 @@
 // Import the mssql package
-var sql = require('mssql');
+//var sql = require('mssql');
 const express = require('express');
 const app = express();
 const port = 3001;
 
-var db = {
+/*var db = {
     server: "pallavi2.database.windows.net", 
     database: "Stopwatch1", 
     user: "azureuser", 
@@ -13,26 +13,26 @@ var db = {
     options: {
         encrypt: true
     }
-};
+};*/
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
-app.get('/api/get', (req, res) => {
+/*app.get('/api/get', (req, res) => {
   var conn = new sql.ConnectionPool(db);
   conn.connect().then(function () {
       var reqst = new sql.Request(conn);
       var spView = 'EXEC ViewTask'
       reqst.query(spView).then(function(result) {
           res.send(result.recordset);
-          /*res.send("This is working!")*/
+          res.send("This is working!")
           conn.close();
       });
   }).catch(function(err) {
       console.log(err);
   });
-});
+});*/
 
 app.listen(port, () => {
   console.log(`running on port ${port}`);
